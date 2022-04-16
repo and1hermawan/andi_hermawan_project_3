0d715f107d6deb85e3b86a80b435029bd5f351c5
unknown <and1haremawan@gmail.com>
2022-04-16 16:24:17 +0700

    district_daily

--- main.py
+++ main.py
@@ -201,6 +201,29 @@ def insert_fact_district_yearly(data, dim_case):
     
     return data
 
+def insert_fact_district_daily(data, dim_case):
+    column_start = ["tanggal", "kode_kab", "suspect_diisolasi", "suspect_discarded", "closecontact_dikarantina", "closecontact_discarded", "probable_diisolasi", "probable_discarded", "confirmation_sembuh", "confirmation_meninggal", "suspect_meninggal", "closecontact_meninggal", "probable_meninggal"]
+    column_end = ['month', 'district_id', 'status', 'total']
+
+    # AGGREGATE
+    data = data[column_start]
+    data = data.melt(id_vars=["tanggal", "kode_kab"], var_name="status", value_name="total").sort_values(["tanggal", "kode_kab", "status"])
+    data = data.groupby(by=['tanggal', 'kode_kab', 'status']).sum()
+    data = data.reset_index()
+
+    # REFORMAT
+    data.columns = column_end
+    data['id'] = np.arange(1, data.shape[0]+1)
+
+    # MERGE
+    dim_case = dim_case.rename({'id': 'case_id'}, axis=1)
+    data = pd.merge(data, dim_case, how='inner', on='status')
+
+    data = data[['id', 'district_id', 'case_id', 'month', 'total']]
+    
+    return data
+
+
 
 def insert_raw_to_warehouse(schema):
     mysql_auth = MySQL(credential['mysql_lake'])
@@ -221,6 +244,8 @@ def insert_raw_to_warehouse(schema):
     fact_province_yearly = insert_fact_province_yearly(data, dim_case)
     fact_district_monthly = insert_fact_district_monthly(data, dim_case)
     fact_district_yearly = insert_fact_district_yearly(data, dim_case)
+    fact_district_yearly = insert_fact_district_daily(data, dim_case)
+    fact_district_daily = insert_fact_district_daily(data, dim_case)
 
     postgre_auth = PostgreSQL(credential['postgresql_warehouse'])
     engine, engine_conn = postgre_auth.connect(conn_type='engine')
@@ -234,7 +259,7 @@ def insert_raw_to_warehouse(schema):
     fact_province_yearly.to_sql('fact_province_yearly', schema=schema, con=engine, index=False, if_exists='replace')
     fact_district_monthly.to_sql('fact_district_monthly', schema=schema, con=engine, index=False, if_exists='replace')
     fact_district_yearly.to_sql('fact_district_yearly', schema=schema, con=engine, index=False, if_exists='replace')
-
+    fact_district_daily.to_sql('fact_district_daily', schema=schema, con=engine, index=False, if_exists='replace')
     engine.dispose()
 
 
